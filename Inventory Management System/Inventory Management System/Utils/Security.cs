f155094ff8b07b560def96523e770e5a70ac1e17
Niko Lepka <slench102+git@gmail.com>
2015-10-20 12:09:32 +0200

    updates the namespaces

--- Inventory Management System/Inventory Management System/Utils/Security.cs
+++ Inventory Management System/Inventory Management System/Utils/Security.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ER = Inventory_Management_System.Models.EmployeeResponsibilities;
+using ED = Inventory_Management_System.Models.EmployeeData;
 
 namespace Inventory_Management_System.Utils
 {
     public static class Security
     {
         
-        bool HasAccess(Employee emp, IResponsibilities res)
+        bool HasAccess(ED.Employee emp, ER.IResponsibilities res)
         {
             throw new NotImplementedException("TBD");
         }
