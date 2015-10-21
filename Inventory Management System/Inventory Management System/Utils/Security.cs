6f5bb244c23c22651b962fcb3290f5f74d003a41
Niko Lepka <slench102+git@gmail.com>
2015-10-21 10:16:46 +0200

    adds string param checker

--- Inventory Management System/Inventory Management System/Utils/Security.cs
+++ Inventory Management System/Inventory Management System/Utils/Security.cs
@@ -9,8 +9,23 @@ namespace Inventory_Management_System.Utils
 {
     public static class Security
     {
-        
-        bool HasAccess(ED.Employee emp, ER.IResponsibility res)
+        public static void ThrowIfAnyEmpty(params string[] input)
+        {
+            foreach (var str in input)
+            {
+                if (String.IsNullOrEmpty(str)) throw new ArgumentException("Input was null or empty");
+            }
+        }
+
+        public static string AddIfNotEmpty(string input)
+        {
+            if (String.IsNullOrEmpty(input)) 
+                throw new ArgumentException("The input was empty");
+            else 
+                return input;
+        }
+
+        public static bool HasAccess(ED.Employee emp, ER.IResponsibility res)
         {
             return emp.Responsibilities.Contains(res);
         }
