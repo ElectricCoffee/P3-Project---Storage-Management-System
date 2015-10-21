acfd7448d9b824f26f2292006d220d240c95aca5
Saveyy <Mads.engelbreth@gmail.com>
2015-10-21 10:15:33 +0200

    Tech, warhouseemployee, warehousemanager har fået restrictions. Buyer er oprettet og har fået tildelt sine rettigheder. Buyer skal tales om

--- Inventory Management System/Inventory Management System/Models/Employee/EmployeeData/WarehouseEmployee.cs
+++ Inventory Management System/Inventory Management System/Models/Employee/EmployeeData/WarehouseEmployee.cs
@@ -3,6 +3,8 @@ using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.ComponentModel;
+using System.Collections.Generic;
+using ER = Inventory_Management_System.Models.EmployeeResponsibilities;
 
 namespace Inventory_Management_System.Models.EmployeeData
 {
@@ -12,6 +14,23 @@ namespace Inventory_Management_System.Models.EmployeeData
         public WarehouseEmployee (string name, string password, string username) 
             : base(name, password, username) 
         {
+            Responsibilities = new List<ER.IResponsibility>
+            {
+                new ER.ArticleNumber1       {ReadWrite = true},
+                new ER.ArticleNumber2       {ReadWrite = true},
+                new ER.Name                 {ReadWrite = false},
+                new ER.SerialNumber         {ReadWrite = true},
+                new ER.WorldLocation        {ReadWrite = true},
+                new ER.InventoryLocation    {ReadWrite = true},
+                new ER.Transit              {ReadWrite = true},
+                new ER.InventoryStatus      {ReadWrite = true},
+                new ER.Tags                 {ReadWrite = true},
+                new ER.Category             {ReadWrite = true},
+                new ER.Images               {ReadWrite = true},
+                new ER.Model                {ReadWrite = true},
+                new ER.Buyer                {ReadWrite = false},
+                new ER.Documents            {ReadWrite = true}
+            };
             if (/* valid indput check her*/true)
             {
                 
