38f2669041ccf21153e186d773f26d160db48126
Niko Lepka <slench102+git@gmail.com>
2015-10-26 14:44:19 +0100

    Implements basic destination parser

--- Inventory Management System/Inventory Management System/Utils/TextParser.cs
+++ Inventory Management System/Inventory Management System/Utils/TextParser.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Inventory_Management_System.Models.Employee;
+using Inventory_Management_System.Models.Message;
 
 namespace Inventory_Management_System.Utils
 {
     public static class TextParser
     {
-        
+        /// <summary>
+        /// Checks if the Leading letter of a targetID is E or R
+        /// Returns the corresponding DestinationType
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        DestinationType DetermineDestination(EmployeeMessage msg)
+        {
+            var destKey = Char.ToUpper(msg.TargetID[0]);
+            return destKey == 'E'
+                ?  DestinationType.Employee
+                :  destKey == 'R' // if not E, check R
+                ?  DestinationType.Responsibility
+                :  DestinationType.Undefined; // if neither E or R, send undefined
+        }
     }
 }
