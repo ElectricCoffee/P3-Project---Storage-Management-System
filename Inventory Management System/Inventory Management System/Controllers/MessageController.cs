212d0631be72136f2bf0b35a34f41f7f96670380
mortenm12 <mortenm12@hotmail.com>
2015-10-26 14:05:03 +0100

    Tilføjer Textparser

--- Inventory Management System/Inventory Management System/Controllers/MessageController.cs
+++ Inventory Management System/Inventory Management System/Controllers/MessageController.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Inventory_Management_System.Models.EmployeeData;
 
 namespace Inventory_Management_System.Controllers
 {
     public class MessageController : ApiController
     {
+        
     }
+
+    
 }
--- Inventory Management System/Inventory Management System/Models/Message/EmployeeMessage.cs
+++ Inventory Management System/Inventory Management System/Models/Message/EmployeeMessage.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Inventory_Management_System.Models.EmployeeData;
 
 namespace Inventory_Management_System.Models.Message
 {
-    public abstract class EmployeeMessage
+    public class EmployeeMessage
     {
+        public string TargetID {get; private set; }
+        public string Message { get; private set; }
+        public string ProductID { get; private set; }
+        public string SenderID { get; private set; }
+
+
+        public EmployeeMessage(string targetId, string senderId, string productId, string message)
+        {
+            TargetID = targetId;
+            Message = message;
+            ProductID = productId;
+            SenderID = senderId;
+        }
     }
 }
