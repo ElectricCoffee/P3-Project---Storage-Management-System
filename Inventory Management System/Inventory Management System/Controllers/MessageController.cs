3a5bfbf2bd6109b8785158f418d1b97f9559164b
mortenm12 <mortenm12@hotmail.com>
2015-10-26 14:11:04 +0100

    Tilføjer MessageControler method

--- Inventory Management System/Inventory Management System/Controllers/MessageController.cs
+++ Inventory Management System/Inventory Management System/Controllers/MessageController.cs
@@ -5,12 +5,36 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Inventory_Management_System.Models.EmployeeData;
+using Inventory_Management_System.Models.Message;
 
 namespace Inventory_Management_System.Controllers
 {
     public class MessageController : ApiController
     {
-        
+        public EmployeeMessage SendMessage(EmployeeMessage)
+        {
+
+        }
+
+        public Queue<Target> EnQueue(EmployeeMessage)
+        {
+
+        }
+
+        public EmployeeMessage DeQueue()
+        {
+
+        }
+
+        public EmployeeMessage ReciveMessage(EmployeeMessage)
+        {
+
+        }
+
+        Queue<Target> SendQueue(Target)
+        {
+
+        }
     }
 
     
