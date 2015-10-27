d7f4a95bc8f4f3ab5cdddd8845ab3853d50a9ba9
Magooyuian <dond9220@gmail.com>
2015-10-27 10:17:17 +0100

    adds log classes and controller

---
+++ Inventory Management System/Inventory Management System/Utils/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_Management_System.Utils
+{
+    public class Log
+    {
+        public Log()
+        {
+
+        }
+
+        private int LogId { get; set; }
+    }
+}
