be1c02fc7c15b1352cbf136057fb4d4fa37ecf3b
JakobKoch <koch.jakob.94@gmail.com>
2015-10-27 11:15:49 +0100

    Implements System and creates helper classes to beautify constructors in Product and System.

--- Inventory Management System/Inventory Management System/Models/Product/Product.cs
+++ Inventory Management System/Inventory Management System/Models/Product/Product.cs
@@ -7,34 +7,57 @@ namespace Inventory_Management_System.Models.Product
 {
     public abstract class Product
     {
-        public Product(int articlenumber1, string name, string worldlocation, string inventorylocation,
-            int amount, int aprice, string tags, string catagory, int salesprice) 
+        public Product(int amount, Label label, Price price, Location location) 
         {
-            if (Security.AnyNullOrEmpty(articlenumber1, name, worldlocation,
-                inventorylocation, amount, tags, catagory, aprice, salesprice))
+            if (Security.AnyNullOrEmpty(label.ArticleNumber1, label.Name, location.WorldLocation,
+                location.InventoryLocation, amount, label.Tags, label.Catagory, price.AcquisitionPrice, price.SalesPrice))
             {
                 throw new ArgumentNullException("Du det dårligste menneske jeg kender.");
             }
-            ArticleNumber1 = articlenumber1;
-            Name = name;
-            Worldlocation = worldlocation;
-            InventoryLocation = inventorylocation;
+            ArticleNumber1 = label.ArticleNumber1;
+            Name = label.Name;
+            WorldLocation = location.WorldLocation;
+            InventoryLocation = location.InventoryLocation;
             Amount = amount;
-            APrice = aprice;
-            Tags = tags;
-            Catagory = catagory;
-            SalesPrice = salesprice;
+            AcquisitionPrice = price.AcquisitionPrice;
+            Tags = label.Tags;
+            Catagory = label.Catagory;
+            SalesPrice = price.SalesPrice;
         }
 
         public int ArticleNumber1 { get; set; }
-        public int ArticleNumber2 { get; set; }
         public string Name { get; set; }
-        public string Worldlocation { get; set; }
+        public string WorldLocation { get; set; }
         public string InventoryLocation { get; set; }
         public int Amount { get; set; }
-        public int APrice { get; set; }
+        public int AcquisitionPrice { get; set; }
         public string Tags { get; set; }
         public string Catagory { get; set; }
         public int SalesPrice { get; set; }
     }
+
+    #region helper classes
+    public class Location
+    {
+        public string WorldLocation { get; set; }
+        public string InventoryLocation { get; set; }
+    }
+
+    public class Price
+    {
+        public int AcquisitionPrice { get; set; }
+        public int SalesPrice { get; set; }
+    }
+
+    public class Label
+    {
+        public int ArticleNumber1 { get; set; }
+        public string Tags { get; set; }
+        public string Catagory { get; set; }
+        public string Name { get; set; }
+    }
+
+    #endregion
 }
+
+
--- Inventory Management System/Inventory Management System/Models/Product/System.cs
+++ Inventory Management System/Inventory Management System/Models/Product/System.cs
@@ -3,11 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Inventory_Management_System.Utils;
 
 namespace Inventory_Management_System.Models.Product
 {
+
+    public enum TransitType { Inbound, Outbound, None }
+
     class System : Product
     {
-        public int SerialNumber
+        public System(Id id, Location location, int amount, Price price, string acquisitor,
+            Status status) 
+            : base(amount, id, price, location)
+        {
+            if (Security.AnyNullOrEmpty(id.ArticleNumber1, id.SerialNumber, id.Name, id.Tags, 
+                id.Catagory, id.Model, id.ProductionYear, location.WorldLocation, location.InventoryLocation, 
+                amount, price.AcquisitionPrice, price.SalesPrice, status.Transit, status.InventoryStatus, 
+                status.SalesStatus, acquisitor))
+            {
+                throw new ArgumentNullException("Du det dårligste menneske jeg kender.");
+            }
+
+            ArticleNumber2 = id.ArticleNumber2;
+            SerialNumber = id.SerialNumber;
+            Transit = status.Transit;
+            InventoryStatus = status.InventoryStatus;
+            SalesStatus = status.SalesStatus;
+            Model = id.Model;
+            ProductionYear = id.ProductionYear;
+            Acquisitor = acquisitor;   
+        }
+
+        public int ArticleNumber2 { get; set; }
+        public int SerialNumber { get; set; }
+        public TransitType Transit { get; set; }
+        public string InventoryStatus { get; set; }
+        public string SalesStatus { get; set; }
+        public string Model { get; set; }
+        public int ProductionYear { get; set; }
+        public string Acquisitor { get; set; }
+
     }
+    #region Helper classes
+    public class Id : Label
+    {
+        public int ArticleNumber2 { get; set; }
+        public int SerialNumber { get; set; }
+        public string Model { get; set; }
+        public int ProductionYear { get; set; }
+    }
+
+    public class Status
+    {
+        public TransitType Transit { get; set; }
+        public string InventoryStatus { get; set; }
+        public string SalesStatus { get; set; }
+    }
+    #endregion
 }
