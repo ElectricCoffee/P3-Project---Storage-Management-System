66eb3934b9bd6084ecf887d9308d4b87022daa6e
Niko Lepka <slench102+git@gmail.com>
2015-10-28 12:56:48 +0100

    Changes semantics, and adds documentation

--- Inventory Management System/Inventory Management System/Utils/Guid.cs
+++ Inventory Management System/Inventory Management System/Utils/Guid.cs
@@ -3,28 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Runtime.InteropServices;
+using MsGuid = System.Guid;
 
 namespace Inventory_Management_System.Utils
 {
     public static class Guid
     {
-        // importing required DLL for UUID generation
+        /// <summary>
+        /// importing required DLL for UUID generation
+        /// creating new UUID in sequencial order.
+        /// </summary>
+        /// <param name="guid">GUID output</param>
+        /// <returns></returns>
         [DllImport("rpcrt4.dll", SetLastError = true)]
-        // creating new UUID in sequencial.
-        static extern int UuidCreateSequential(out System.Guid guid);
-   
+        private static extern int UuidCreateSequential(out MsGuid guid);
 
-        public static System.Guid NewGuid()
+        /// <summary>
+        /// Creates a string-version of the GUID
+        /// </summary>
+        /// <returns></returns>
+        public static String CreateString()
         {
-            return CreateSequentialUuid();
+            return CreateSequential().ToString();
         }
 
-        public static System.Guid CreateSequentialUuid()
+        /// <summary>
+        /// Creates a new sequential GUID
+        /// </summary>
+        /// <returns></returns>
+        public static MsGuid CreateSequential()
         {
-            const int RPC_S_OK = 0;
-            System.Guid g;
-            int hr = UuidCreateSequential(out g);
-            if (hr != RPC_S_OK)
+            MsGuid g;
+            int ok = 0, hr = UuidCreateSequential(out g);
+            if (hr != ok)
                 throw new ApplicationException("UuidCreateSequential failed: " + hr);
             return g;
         }
