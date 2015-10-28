b89fa65f211cbd9e01219f977e10bab51f2a3831
Niko Lepka <slench102+git@gmail.com>
2015-10-28 12:24:31 +0100

    tries to do clever shit

--- Inventory Management System/Inventory Management System/Controllers/MessageController.cs
+++ Inventory Management System/Inventory Management System/Controllers/MessageController.cs
@@ -14,57 +14,38 @@ namespace Inventory_Management_System.Controllers
 {
     public class MessageController : ApiController
     {
+        // get all the messages
+        public void Get() { }
+        // get all messages defined by a specific target
+        public void Get([FromUri] Target target) { }
+        public void Put() { }
+        public void Post() { }
+        public void Delete() { }
 
-        public List<Queue<EmployeeMessage>> MessageQueue = new List<Queue<EmployeeMessage>>();  //en liste af køer af meddelser
+        //public EmployeeMessage SendMessage([FromBody] EmployeeMessage msg)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        /// <summary>
-        /// Send Message to the employee(s) and add's the message to the queue og the rolegroup.
-        /// </summary>
-        /// <param name="msg">The message, wich have the target employee.</param>
-        /// <returns></returns>
-        public EmployeeMessage SendMessage(EmployeeMessage msg)
-        {
-            throw new NotImplementedException();
-        }
+        //public Queue<Target> EnQueue([FromBody] EmployeeMessage msg, [FromUri] Target target)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        /// <summary>
-        /// Add a message to the queue of the target rolegroup.
-        /// </summary>
-        /// <param name="msg">The message, wich have the target rolegroup.</param>
-        /// <returns></returns>
-        public Queue<Target> EnQueue(EmployeeMessage msg)
-        {
-            throw new NotImplementedException();
-        }
+        //public EmployeeMessage DeQueue()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        /// <summary>
-        /// Dequeue the first added message to the queue.
-        /// </summary>
-        /// <returns>returns the first added message.</returns>
-        public EmployeeMessage DeQueue()
-        {
-            throw new NotImplementedException();
-        }
+        //public EmployeeMessage ReciveMessage([FromBody] EmployeeMessage msg)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        /// <summary>
-        /// Recive a message from the employee and make sure to resend the message to the right employee.
-        /// </summary>
-        /// <param name="msg">the message wich contain the target employee(s).</param>
-        /// <returns></returns>
-        public EmployeeMessage ReciveMessage(EmployeeMessage msg)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Gets the hole queue of a rolegroup.
-        /// </summary>
-        /// <param name="target">the target rolegroup.</param>
-        /// <returns>Returns the hole queue.</returns>
-        Queue<Target> SendQueue(Target target)
-        {
-            throw new NotImplementedException();
-        }
+        //Queue<Target> SendQueue([FromUri] Target target)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 
     
