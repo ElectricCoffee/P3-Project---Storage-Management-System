279c9d82b5a87893f408d1bdad388141c4f28272
JakobKoch <koch.jakob.94@gmail.com>
2015-10-28 10:10:02 +0100

    Implements file load methods

--- Inventory Management System/Inventory Management System/Models/Product/System.cs
+++ Inventory Management System/Inventory Management System/Models/Product/System.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Inventory_Management_System.Utils;
+using System.Drawing;
+using System.IO;
 
 namespace Inventory_Management_System.Models.Product
 {
@@ -13,7 +15,8 @@ namespace Inventory_Management_System.Models.Product
     class System : Product
     {
         public System(Id id, Location location, int amount, Price price, string acquisitor,
-            Status status)
+            Status status, string documentDirectory, string documentName,
+            string specsheetDirectory, string specsheetName, string imageDirectory, string imageName)
             : base(amount, id, price, location)
         {
             if (Security.AnyNullOrEmpty(id.ArticleNumber1, id.SerialNumber, id.Name, id.Tags,
@@ -32,7 +35,44 @@ namespace Inventory_Management_System.Models.Product
             Model = id.Model;
             ProductionYear = id.ProductionYear;
             Acquisitor = acquisitor;
+
+
+#if DEBUG
+#warning - Skal lige kvalitet checkes, da jeg ikke ved om dette er okay.
+            if (imageName != null && imageDirectory != null)
+            {
+                Images = (Bitmap)Image.FromFile(imageDirectory + @"\" + imageName);
+            }
+            if (specsheetDirectory != null && specsheetName != null)
+            {
+                loadspecsheet(specsheetDirectory, specsheetName);
+            }
+            if (documentDirectory != null && documentName != null)
+            {
+                loadDocument(documentDirectory, documentName);
+            }
+#endif
         }
+
+        private void loadDocument(string MainDirectory, string filename)
+        {
+            string filsti = MainDirectory + @"\" + filename + ".txt";
+            foreach (var s in filsti)
+            {
+                FDocuments.Add(s.ToString());
+            }
+        }
+
+        private void loadspecsheet(string MainDirectory, string filename)
+        {
+            string filsti = MainDirectory + @"\" + filename + ".txt";
+            foreach (var s in filsti)
+            {
+                SpecSheet.Add(s.ToString());
+            }
+        }
+
+
 
         public int ArticleNumber2 { get; set; }
         public int SerialNumber { get; set; }
@@ -43,22 +83,26 @@ namespace Inventory_Management_System.Models.Product
         public int ProductionYear { get; set; }
         public string Acquisitor { get; set; }
         public List<SparePart> Parts { get; set; }
-    }
+        public Bitmap Images { get; set; }
+        public List<string> SpecSheet { get; set; }
+        public List<string> FDocuments { get; set; }
 
-    #region Helper classes
-    public class Id : Label
-    {
-        public int ArticleNumber2 { get; set; }
-        public int SerialNumber { get; set; }
-        public string Model { get; set; }
-        public int ProductionYear { get; set; }
-    }
+        #region Helper classes
+        public class Id : Label
+        {
+            public int ArticleNumber2 { get; set; }
+            public int SerialNumber { get; set; }
+            public string Model { get; set; }
+            public int ProductionYear { get; set; }
+        }
 
-    public class Status
-    {
-        public TransitType Transit { get; set; }
-        public string InventoryStatus { get; set; }
-        public string SalesStatus { get; set; }
+        public class Status
+        {
+            public TransitType Transit { get; set; }
+            public string InventoryStatus { get; set; }
+            public string SalesStatus { get; set; }
+        }
+
+        #endregion
     }
-    #endregion
 }
