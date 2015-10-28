eeca3b5b05012072309cddce5819e60d5841092c
mortenm12 <mortenm12@hotmail.com>
2015-10-28 14:11:44 +0100

    Tilføjer Log event

---
+++ Inventory Management System/Inventory Management System/Utils/LogEventStatusChange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Inventory_Management_System.Models.EmployeeData;
+
+namespace Inventory_Management_System.Utils
+{
+    public class LogEventStatusChange : LogEvent
+    {
+        public string OldStatus { get; private set; }
+        public string NewStatus { get; private set; }
+
+        public override string ToString()
+        {
+            return "Employee: " + Employee + " Time: " + Time + " Old Status: " + OldStatus + " New Status: " + NewStatus;
+        }
+
+        public LogEventStatusChange(Employee employee, DateTime time, string oldStatus, string newStatus)
+        {
+            Employee = employee;
+            Time = time;
+            OldStatus = oldStatus;
+            NewStatus = newStatus;
+        }
+    }
+}
