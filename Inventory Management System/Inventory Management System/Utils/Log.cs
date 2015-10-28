476381df31d65bdda8c9f97bac5f110140a9ada1
Daniel <dond9220@hotmail.com>
2015-10-28 13:15:26 +0100

    adds more funktionality to log class

--- Inventory Management System/Inventory Management System/Utils/Log.cs
+++ Inventory Management System/Inventory Management System/Utils/Log.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Inventory_Management_System.Models.Product;
+using Inventory_Management_System.Models.EmployeeData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,14 +9,29 @@ namespace Inventory_Management_System.Utils
 {
     public class Log
     {
-        public Log()
+        public Log(Product product,Employee employee,/* LogEvent event,*/ string comment)
         {
+            try
+            {
+                LogId = Guid.CreateString();
+            }
+            catch (ApplicationException)
+            {
 
+                throw;
+            }
+            LogId = 
             Time = DateTime.Now;
+            ProductId = product.ArticleNumber1;
+            Employe = employee.ToString();
+            Comment = comment;
         }
 
-        public string LogId { get { return Guid.NewGuid().ToString(); } }
+        public string LogId { get; private set;}
         public DateTime Time { get; private set; }
-        
+        public string Employe { get; set;}
+        public int ProductId { get; set;}
+        public string Comment { get; set;}
+
     }
 }
