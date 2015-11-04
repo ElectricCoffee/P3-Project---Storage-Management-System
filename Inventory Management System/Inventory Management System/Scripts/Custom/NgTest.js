d66f4e7053fc66b9b254f37bf26df35516fd0e43
Niko Lepka <slench102+git@gmail.com>
2015-11-04 06:26:31 +0100

    Implements coffeescript as javascript due to complications with coffeescript's language features

---
+++ Inventory Management System/Inventory Management System/Scripts/Custom/NgTest.js
@@ -0,0 +1,52 @@
+﻿(function() {
+    'use strict';
+    angular
+        .module('custom', ['SignalR'])
+        .service('MessageService', MessageService)
+        .controller('CustomController', CustomController)
+        .controller('MessageController', MessageController);
+
+    // dependency injections
+    MessageService.$inject = ['$rootScope', 'Hub', '$timeout'];
+    MessageController.$inject = ['MessageService'];
+
+    // constructors
+    function CustomController() {
+        this.greeting = 'hello, Angular.js!';
+    }
+
+    function Message(sender, message) {
+        this.message = message;
+        this.sender = sender;
+    }
+
+    function MessageService($rootScope, Hub, $timeout) {
+        var self = this; // define alias
+        var hub = new Hub('message', {
+            // client-side methods
+            listeners: { // listensers 'listen' for messages from the server
+                'displayMessage': function (sndr, msg) {
+                    pushMessage(sndr, msg);
+                    $rootScope.$apply();
+                }
+            },
+            // server-side methods
+            methods: ['sendMessage'],
+            errorHandler: err => console.error(err)
+        });
+
+        // Angular methods
+        self.allMessages = []
+        self.push = pushMessage;
+        self.send = hub.sendMessage;
+
+         function pushMessage (sndr, msg) {
+            self.allMessages.push(new Message(sndr, msg));
+        };
+    }
+
+    function MessageController(MessageService) {
+        this.messages = MessageService.allMessages;
+        this.sendMessage = MessageService.send;
+    }
+})();
