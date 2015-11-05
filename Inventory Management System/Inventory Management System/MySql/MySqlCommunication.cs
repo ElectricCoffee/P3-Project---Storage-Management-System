02ed2f5615ae45e5b1d207666ab226744195ba21
mortenm12 <mortenm12@hotmail.com>
2015-11-05 12:23:11 +0100

    Tilføjer klassen MySqlCummunication og funktionen send

---
+++ Inventory Management System/Inventory Management System/MySql/MySqlCommunication.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Inventory_Management_System.MySql
+{
+    public class MySqlCommunication
+    {
+        private string connectionstring = "Server=localhost; Port=50309; Database=lbn_medical_db; Uid=root; Pwd=DS309e15LS;"; //fastkodning ikke en god ide
+        private MySqlConnection connection;
+        private MySqlCommand cmd;
+
+
+        public MySqlCommunication()
+        {
+            connection = new MySqlConnection(connectionstring);
+            
+        }
+
+        public string SendString(string text)
+        {
+            try
+            {
+                connection.Open();
+                cmd = connection.CreateCommand();
+                cmd.CommandText = text;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+            finally
+            {
+                if(connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+                
+            }
+            return text;
+
+        }
+    }
+}
