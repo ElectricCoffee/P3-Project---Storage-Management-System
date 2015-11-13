4bcf7e219948c279bdde4eaf2525525a90e0a1eb
Daniel <dond9220@hotmail.com>
2015-11-13 10:01:44 +0100

    adds addproduct gui more or less

--- Inventory Management System/Inventory Management System/Controllers/InvEmpController.cs
+++ Inventory Management System/Inventory Management System/Controllers/InvEmpController.cs
@@ -13,5 +13,9 @@ namespace Inventory_Management_System.Controllers
         {
             return View();
         }
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
     }
 }
