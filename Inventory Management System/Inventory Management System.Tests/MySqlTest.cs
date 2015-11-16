6e0046ed863a0f53b38abf2bb90c990089766aca
mortenm12 <mortenm12@hotmail.com>
2015-11-16 10:33:45 +0100

    Forsæter på unittest

--- Inventory Management System/Inventory Management System.Tests/MySqlTest.cs
+++ Inventory Management System/Inventory Management System.Tests/MySqlTest.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Inventory_Management_System.MySql;
 
 namespace Inventory_Management_System.Tests
 {
-    class MySqlTest
+    [TestClass]
+    public class MySqlTest
     {
+
+        [TestMethod]
+        public void SendSTringTest()
+        {
+            string text = String.Format("INSERT INTO {0}(Username) VALUE('Morten')", MySqlCommunication.EmployeeTable);
+            Assert.AreEqual(text, MySqlCommunication.SendString(text));
+        }
+
+        [TestMethod]
+        public void GetStringTest()
+        {
+            string text = String.Format("SELECT Username FROM {0} WHERE Username = Morten", MySqlCommunication.EmployeeTable);
+            Assert.AreEqual("Morten", MySqlCommunication.GetString(text));
+        }
     }
 }
