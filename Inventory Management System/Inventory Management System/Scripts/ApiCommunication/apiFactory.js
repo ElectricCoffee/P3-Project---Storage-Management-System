017a2adcbd31de45c7fd90cd5011dc7df403cb38
Niko Lepka <slench102+git@gmail.com>
2015-11-16 18:48:05 +0100

    adds documentation code will throw errors instead of printing to the console

--- Inventory Management System/Inventory Management System/Scripts/ApiCommunication/apiFactory.js
+++ Inventory Management System/Inventory Management System/Scripts/ApiCommunication/apiFactory.js
@@ -1,9 +1,12 @@
-﻿(function () {
+﻿// NOTE: ALL OF THE FOLLOWING METHODS RETURN A PROMISE
+//       SEE https://docs.angularjs.org/api/ng/service/$http FOR MORE INFO
+
+(function () {
     'use strict';
     angular.module('apiCommunication')
         .service('ApiFactory', ApiFactory);
 
-    ApiFactory.$inject = ["$http"];
+    ApiFactory.$inject = ['$http'];
 
     function ApiFactory($http) {
         return function (controllerName) {
@@ -11,28 +14,42 @@
             this.create = post;
             this.read   = get;
             this.update = put;
-            this.delete = del;
+            this.remove = del; // called remove instead of delete to not clash with the delete keyword
 
+            // sends a post request to the server at the specified URL
+            // this will create a new entry in the specified database
             function post(obj) {
-                $http.post(url, obj);
+                return $http.post(url, obj);
             }
 
+            // gets data from the server, if called without an argument it gets everything
+            // get() = get everything
+            // get(foo) = get everything witht he ID 'foo'
             function get(id) {
-                var newUrl = id ? url + "/" + id : url;
+                var newUrl = id ? url + '/' + id : url;
                 return $http.get(newUrl);
             }
 
+            // updates an item with the specified id on the server
+            // if neither data nor id exist, an error will be thrown
             function put(id, newData) {
-                var newUrl = url + "/" + id;
-                if (!id) console.error("An id was not provided");
-                else if (!newData) console.error("no data was provided");
-                else return $http.put(newUrl, newData);
+                var newUrl = url + '/' + id;
+                if (!id)
+                    throw new URIError('An id was not provided', 'apiFactory.js');
+                else if (!newData)
+                    throw new URIError('No data was provided', 'apiFactory.js');
+                else
+                    return $http.put(newUrl, newData);
             }
 
+            // delete an item with the specified id from the server
+            // if the id isn't provided, an error will be thrown
             function del(id) {
-                var newUrl = url + "/" + id;
-                if (!id) console.error("An id was not provided");
-                else return $http.delete(newUrl);
+                var newUrl = url + '/' + id;
+                if (!id)
+                    throw new URIError('An id was not provided', 'apiFactory.js');
+                else
+                    return $http.delete(newUrl);
             }
         };
     }
