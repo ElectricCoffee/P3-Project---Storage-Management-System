8c741e4a7ee388dc52041e252a67a1f6c47f7786
Niko Lepka <slench102+git@gmail.com>
2015-11-19 09:39:24 +0100

    Starts merging views and angular

--- Inventory Management System/Inventory Management System/Scripts/Employee/Accountant/controller.js
+++ Inventory Management System/Inventory Management System/Scripts/Employee/Accountant/controller.js
@@ -3,9 +3,11 @@
     angular.module('IMS')
         .controller('AccountantController', AccountantController);
 
+    // dependency injection
     AccountantController.$inject = ['EmployeeService'];
 
+    // controller constructor
     function AccountantController(EmployeeService) {
-
+        this.tableData = [];
     }
 })();
