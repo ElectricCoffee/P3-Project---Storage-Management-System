a60a2ab4ba7bd09b9e362600cdff605f2d41be8e
Saveyy <Mads.engelbreth@gmail.com>
2015-11-24 12:59:04 +0100

    Controller for salesmng +start of inventory

--- Inventory Management System/Inventory Management System/Scripts/Employee/Inventory/controller.js
+++ Inventory Management System/Inventory Management System/Scripts/Employee/Inventory/controller.js
@@ -6,9 +6,12 @@
     InventoryController.$inject = ['EmployeeService', 'ProductService'];
 
     function InventoryController(EmployeeService, ProductService) {
-        this.productService = ProductService;
+        var self = this;
+        self.productService = ProductService;
+        self.addProduct = AddProduct;
+        self.editProduct = EditProduct;
 
-        this.product = {
+        self.product = {
             ArticleNumber: '',
             Name: '',
             SerialNumber: '',
@@ -17,5 +20,23 @@
             Tags: '',
             Comments: ''
         };
+    }
+    function AddProduct() {
+        ProductService.service.post(self.product)
+            .success(function (response) {
+                alert("successfully posted" + JSON.stringify(self.product));
+            })
+            .failure(function (response) {
+                alert("failed to post");
+            })
+    }
+    function EditProduct() {
+        ProductService.service.put(id, self.product) ///id === articlenumber
+            .success(function (response) {
+                alert("successfully put" + JSON.stringify(self.product));
+            })
+            .failure(function (response) {
+                alert("failed to put");
+            })
     }
 })();
---
+++ Inventory Management System/Inventory Management System/Scripts/Employee/SalesMng/Controller.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+    angular.module('IMS')
+        .controller('SalesMngController', SaleMngController);
+
+    SalesMngController.$inject = ['EmployeeService', 'ProductService'];
+
+    function SalesMngController(EmployeeService, ProductService) {
+        this.productService = ProductService;
+    }
+})();
