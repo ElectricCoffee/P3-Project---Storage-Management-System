bdf4d0439818408fcdce13ba337efe0a1c7d8f49
Niko Lepka <slench102+git@gmail.com>
2015-12-03 18:26:25 +0100

    Adds a search bar

---
+++ Inventory Management System/Inventory Management System/Scripts/System/searchBarDirective.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+    angular.module('IMS')
+        .directive('searchBar', searchBar);
+
+    function searchBar() {
+        return {
+            restrict: 'E',
+            //scope: { type: '@', sender: '@' },
+            //transclude: true,
+            template:
+                    '<div class="input-group">' + 
+                        '<span class="input-group-addon glyphicon glyphicon-search"' + 
+                              'aria-hidden="true" id="input-addon"></span>' + 
+                        '<input ng-model="query" placeholder="search"' + 
+                               'aria-describedby="input-addon" class="form-control">' + 
+                    '</div>'
+        };
+    }
+})();
